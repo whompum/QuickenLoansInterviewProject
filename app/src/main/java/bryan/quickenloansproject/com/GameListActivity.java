4f3dfc00cf36af1bfe9480e7616e6ccf88cb02f9
Bryan <whompum101@gmail.com>
2019-01-03 22:22:42 -0500

    Add dependencies; Create Base Ui

---
+++ app/src/main/java/bryan/quickenloansproject/com/GameListActivity.java
@@ -0,0 +1,13 @@
+package bryan.quickenloansproject.com;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class GameListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
